ebfdeb6d0dc6685e0d48a613772a46854b82a00d
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-04-10 17:20:13 +0300

    Add JavaFX example
    Also:
     * Fixed usage of Gradle plugins
     * Fixed Javadoc links

--- quickstart-javafx/src/main/java/HelloWorld.java
+++ quickstart-javafx/src/main/java/HelloWorld.java
@@ -19,10 +19,13 @@
  */
 
 import com.teamdev.jxbrowser.chromium.Browser;
-import com.teamdev.jxbrowser.chromium.swing.BrowserView;
-
-import javax.swing.*;
-import java.awt.*;
+import com.teamdev.jxbrowser.chromium.BrowserCore;
+import com.teamdev.jxbrowser.chromium.internal.Environment;
+import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * A simplest application with integrated browser component.
@@ -30,22 +33,36 @@ import java.awt.*;
  * <p>This example demonstrates:
  * <ol>
  *     <li>Creating an instance of {@link com.teamdev.jxbrowser.chromium.Browser Browser}
- *     <li>Embedding the browser into Swing via {@link com.teamdev.jxbrowser.chromium.swing.BrowserView BrowserView}.
+ *     <li>Embedding the browser into JavaFX via {@link com.teamdev.jxbrowser.chromium.javafx.BrowserView BrowserView}.
  *     <li>Loading HTML content as a string.
  * </ol>
  */
-public class HelloWorld {
-    public static void main(String[] args) {
-        Browser browser = new Browser();
-        BrowserView view = new BrowserView(browser);
+public class HelloWorld extends Application {
+    
+    @Override
+    public void init() throws Exception {
+        // On Mac OS X Chromium engine must be initialized in non-UI thread.
+        if (Environment.isMac()) {
+            BrowserCore.initialize();
+        }
+    }
 
-        JFrame frame = new JFrame("JxBrowser - Hello World");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(view, BorderLayout.CENTER);
-        frame.setSize(500, 400);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    @Override
+    public void start(Stage primaryStage) {
+        Browser browser = new Browser();
+        BrowserView browserView = new BrowserView(browser);
+
+        StackPane pane = new StackPane();
+        pane.getChildren().add(browserView);
+        Scene scene = new Scene(pane, 500, 400);
+        primaryStage.setTitle("JxBrowser: JavaFX - Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
         browser.loadHTML("<html><body><h1>Hello World!</h1></body></html>");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
