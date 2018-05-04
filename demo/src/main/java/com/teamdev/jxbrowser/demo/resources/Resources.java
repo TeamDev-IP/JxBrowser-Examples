65aec0bff077afcd6af64623a2b969274dfe5943
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-05-04 17:19:42 +0300

    Document `Resources`

--- demo/src/main/java/com/teamdev/jxbrowser/demo/resources/Resources.java
+++ demo/src/main/java/com/teamdev/jxbrowser/demo/resources/Resources.java
@@ -7,10 +7,22 @@
 package com.teamdev.jxbrowser.demo.resources;
 
 import javax.swing.ImageIcon;
+import java.net.URL;
 
+/**
+ * Utility class for accessing program resources.
+ */
 public class Resources {
 
+    /** Prevents instantiation of this  utility class. */
+    private Resources() {
+    }
+
+    /**
+     * Loads an {@code ImageIcon} from a resource file.
+     */
     public static ImageIcon getIcon(String fileName) {
-        return new ImageIcon(Resources.class.getResource(fileName));
+        URL resource = Resources.class.getResource(fileName);
+        return new ImageIcon(resource);
     }
 }
