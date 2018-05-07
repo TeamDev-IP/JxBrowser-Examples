2c110dd49a2f3ec14b48a3c6a7bda2babd3cb40f
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-05-07 16:57:14 +0300

    Use ECMAScript 6 for in examples

--- tutorials/content-changes/src/main/resources/observer.js
+++ tutorials/content-changes/src/main/resources/observer.js
@@ -1,9 +1,9 @@
-var element = document.getElementById('counter');
-var observer = new MutationObserver(
+const element = document.getElementById('counter');
+const observer = new MutationObserver(
     function(mutations) {
         window.java.onDomChanged(element.innerHTML);
     });
-var config = {childList: true};
+const config = {childList: true};
 observer.observe(element, config);
 
 
