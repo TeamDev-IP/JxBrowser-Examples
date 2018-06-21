1e10ede61900c470687136a2b853704b11669486
alexey-vexler <alexey.veksler@teamdev.com>
2018-06-21 15:46:58 +0300

    Add source code examples of the JavaScript section.

---
+++ javascript/console-messages/src/main/java/ConsoleListenerExample.java
@@ -0,0 +1,33 @@
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.events.ConsoleEvent;
+import com.teamdev.jxbrowser.chromium.events.ConsoleListener;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The example demonstrates how to listen to console messages including
+ * JavaScript errors.
+ */
+public class ConsoleListenerExample {
+    public static void main(String[] args) {
+        Browser browser = new Browser();
+        BrowserView browserView = new BrowserView(browser);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(browserView, BorderLayout.CENTER);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        browser.addConsoleListener(new ConsoleListener() {
+            public void onMessage(ConsoleEvent event) {
+                System.out.println("Level: " + event.getLevel());
+                System.out.println("Message: " + event.getMessage());
+            }
+        });
+        browser.executeJavaScript("console.error(\"Error message\");");
+    }
+}
