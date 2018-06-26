69200ecc63bb2d57927d71b0f2e3fb6846196755
vladimir ikryanov <vladimir.ikryanov@teamdev.com>
2018-06-26 15:06:25 +0300

    Add the missing resource to the example

--- chromium/switchers/src/main/java/LoadLocalFiles.java
+++ chromium/switchers/src/main/java/LoadLocalFiles.java
@@ -21,31 +21,38 @@
 import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.BrowserPreferences;
 import com.teamdev.jxbrowser.chromium.swing.BrowserView;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.net.URL;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 /**
- * The example demonstrates how to enable access to local files from a web page.
+ * The example demonstrates how to enable access to the local files from a web page.
  */
 public class LoadLocalFiles {
+
     public static void main(String[] args) {
+        // Disable web security for the Chromium engine and allow accessing local files
+        // from a web page.
         BrowserPreferences.setChromiumSwitches(
-            "--disable-web-security",
-            "--allow-file-access-from-files"
+                "--disable-web-security",
+                "--allow-file-access-from-files"
         );
 
         Browser browser = new Browser();
         BrowserView browserView = new BrowserView(browser);
 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("JxBrowser Example – Load Local Files");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(browserView, BorderLayout.CENTER);
         frame.setSize(700, 500);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        browser.loadHTML("<html><body>Image:<img src='file:///C:\\image.jpg'></body></html>");
+        // Get absolute path to the jxbrowser-logo.svg file.
+        URL resource = LoadLocalFiles.class.getResource("jxbrowser-logo.svg");
+
+        // Load HTML with an image located on the local file system.
+        browser.loadHTML("<html><body><img src='" + resource + "'></body></html>");
     }
 }
-
