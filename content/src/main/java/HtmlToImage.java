fae416c9f655a9b88ae71e44aaca8b662416e509
alexey-vexler <alexey.veksler@teamdev.com>
2018-06-30 18:37:31 +0300

    Merge the examples in one "Content" section. Add trailing EOF in all files.

--- content/src/main/java/HtmlToImage.java
+++ content/src/main/java/HtmlToImage.java
@@ -22,34 +22,25 @@ import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.BrowserPreferences;
 import com.teamdev.jxbrowser.chromium.BrowserType;
 import com.teamdev.jxbrowser.chromium.Callback;
-import com.teamdev.jxbrowser.chromium.internal.LightWeightWidgetListener;
-import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
-import com.teamdev.jxbrowser.chromium.javafx.internal.LightWeightWidget;
-import java.awt.Dimension;
-import java.awt.Rectangle;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+import com.teamdev.jxbrowser.chromium.swing.internal.LightWeightWidget;
+import com.teamdev.jxbrowser.chromium.swing.internal.events.LightWeightWidgetListener;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import javax.imageio.ImageIO;
 
 /**
- * The example demonstrates how to get screen shot of the web page
+ * This example demonstrates how to get screen shot of the web page
  * and save it as PNG image file.
  */
-public class JavaFXHTMLToImage extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(final Stage primaryStage) throws Exception {
+public class HtmlToImage {
+    public static void main(String[] args) throws Exception {
         final int viewWidth = 1024;
-        final int viewHeight = 15000;
+        final int viewHeight = 20000;
         // Disables GPU process and changes maximum texture size
         // value from default 16384 to viewHeight. The maximum texture size value
         // indicates the maximum height of the canvas where Chromium
@@ -59,38 +50,35 @@ public class JavaFXHTMLToImage extends Application {
         // black color.
         String[] switches = {
                 "--disable-gpu",
-                "--disable-gpu-compositing",
-                "--enable-begin-frame-scheduling",
                 "--max-texture-size=" + viewHeight
         };
         BrowserPreferences.setChromiumSwitches(switches);
 
-        // #1 Create LIGHTWEIGHT Browser instance
-        final Browser browser = new Browser(BrowserType.LIGHTWEIGHT);
-        final BrowserView view = new BrowserView(browser);
+        // #1 Create LIGHTWEIGHT Browser instance.
+        Browser browser = new Browser(BrowserType.LIGHTWEIGHT);
+        BrowserView view = new BrowserView(browser);
 
-        // #2 Get javafx.scene.image.Image of the loaded web page
-        final LightWeightWidget widget = (LightWeightWidget) view.getChildren().get(0);
-
-        // #3 Register LightWeightWidgetListener.onRepaint() to get
+        // #2 Register LightWeightWidgetListener.onRepaint() to get
         // notifications about paint events. We expect that web page
         // will be completely rendered twice:
         // 1. When its size is updated to viewWidth x viewHeight.
         // 2. When HTML content is loaded and displayed.
         final CountDownLatch latch = new CountDownLatch(2);
+        LightWeightWidget widget = (LightWeightWidget) view.getComponent(0);
         widget.addLightWeightWidgetListener(new LightWeightWidgetListener() {
             @Override
             public void onRepaint(Rectangle updatedRect, Dimension viewSize) {
+                // Make sure that all view content has been repainted.
                 if (viewSize.equals(updatedRect.getSize())) {
                     latch.countDown();
                 }
             }
         });
 
-        // #4 Set the required view size
+        // #3 Set the required view size.
         browser.setSize(viewWidth, viewHeight);
 
-        // #5 Load web page and wait until web page is loaded completely
+        // #4 Load web page and wait until web page is loaded completely.
         Browser.invokeAndWaitFinishLoadingMainFrame(browser, new Callback<Browser>() {
             @Override
             public void invoke(Browser browser) {
@@ -98,18 +86,14 @@ public class JavaFXHTMLToImage extends Application {
             }
         });
 
-        // #6 Wait until Chromium renders web page content.
+        // #5 Wait until Chromium renders web page content.
         latch.await(45, TimeUnit.SECONDS);
 
-        final Image image = widget.getImage();
+        // #6 Save java.awt.Image of the loaded web page into a PNG file.
+        ImageIO.write((RenderedImage) widget.getImage(), "PNG",
+                new File("teamdev.com.png"));
 
-        // #7 Save the image into a PNG file
-        ImageIO.write(SwingFXUtils.fromFXImage(image, null), "PNG", new File("teamdev.com.png"));
-
-        // #8 Dispose Browser instance.
+        // #7 Dispose Browser instance.
         browser.dispose();
-
-        // #9 Close the application.
-        Platform.exit();
     }
 }
--- content/src/main/java/JavaFxHtmlToImage.java
+++ content/src/main/java/JavaFxHtmlToImage.java
@@ -22,25 +22,34 @@ import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.BrowserPreferences;
 import com.teamdev.jxbrowser.chromium.BrowserType;
 import com.teamdev.jxbrowser.chromium.Callback;
-import com.teamdev.jxbrowser.chromium.swing.BrowserView;
-import com.teamdev.jxbrowser.chromium.swing.internal.LightWeightWidget;
-import com.teamdev.jxbrowser.chromium.swing.internal.events.LightWeightWidgetListener;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.RenderedImage;
+import com.teamdev.jxbrowser.chromium.internal.LightWeightWidgetListener;
+import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
+import com.teamdev.jxbrowser.chromium.javafx.internal.LightWeightWidget;
+import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.io.File;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
- * This example demonstrates how to get screen shot of the web page
+ * The example demonstrates how to get screen shot of the web page
  * and save it as PNG image file.
  */
-public class HTMLToImage {
-    public static void main(String[] args) throws Exception {
+public class JavaFxHtmlToImage extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
         final int viewWidth = 1024;
-        final int viewHeight = 20000;
+        final int viewHeight = 15000;
         // Disables GPU process and changes maximum texture size
         // value from default 16384 to viewHeight. The maximum texture size value
         // indicates the maximum height of the canvas where Chromium
@@ -50,35 +59,38 @@ public class HTMLToImage {
         // black color.
         String[] switches = {
                 "--disable-gpu",
+                "--disable-gpu-compositing",
+                "--enable-begin-frame-scheduling",
                 "--max-texture-size=" + viewHeight
         };
         BrowserPreferences.setChromiumSwitches(switches);
 
-        // #1 Create LIGHTWEIGHT Browser instance.
-        Browser browser = new Browser(BrowserType.LIGHTWEIGHT);
-        BrowserView view = new BrowserView(browser);
+        // #1 Create LIGHTWEIGHT Browser instance
+        final Browser browser = new Browser(BrowserType.LIGHTWEIGHT);
+        final BrowserView view = new BrowserView(browser);
 
-        // #2 Register LightWeightWidgetListener.onRepaint() to get
+        // #2 Get javafx.scene.image.Image of the loaded web page
+        final LightWeightWidget widget = (LightWeightWidget) view.getChildren().get(0);
+
+        // #3 Register LightWeightWidgetListener.onRepaint() to get
         // notifications about paint events. We expect that web page
         // will be completely rendered twice:
         // 1. When its size is updated to viewWidth x viewHeight.
         // 2. When HTML content is loaded and displayed.
         final CountDownLatch latch = new CountDownLatch(2);
-        LightWeightWidget widget = (LightWeightWidget) view.getComponent(0);
         widget.addLightWeightWidgetListener(new LightWeightWidgetListener() {
             @Override
             public void onRepaint(Rectangle updatedRect, Dimension viewSize) {
-                // Make sure that all view content has been repainted.
                 if (viewSize.equals(updatedRect.getSize())) {
                     latch.countDown();
                 }
             }
         });
 
-        // #3 Set the required view size.
+        // #4 Set the required view size
         browser.setSize(viewWidth, viewHeight);
 
-        // #4 Load web page and wait until web page is loaded completely.
+        // #5 Load web page and wait until web page is loaded completely
         Browser.invokeAndWaitFinishLoadingMainFrame(browser, new Callback<Browser>() {
             @Override
             public void invoke(Browser browser) {
@@ -86,14 +98,18 @@ public class HTMLToImage {
             }
         });
 
-        // #5 Wait until Chromium renders web page content.
+        // #6 Wait until Chromium renders web page content.
         latch.await(45, TimeUnit.SECONDS);
 
-        // #6 Save java.awt.Image of the loaded web page into a PNG file.
-        ImageIO.write((RenderedImage) widget.getImage(), "PNG",
-                new File("teamdev.com.png"));
+        final Image image = widget.getImage();
 
-        // #7 Dispose Browser instance.
+        // #7 Save the image into a PNG file
+        ImageIO.write(SwingFXUtils.fromFXImage(image, null), "PNG", new File("teamdev.com.png"));
+
+        // #8 Dispose Browser instance.
         browser.dispose();
+
+        // #9 Close the application.
+        Platform.exit();
     }
 }
