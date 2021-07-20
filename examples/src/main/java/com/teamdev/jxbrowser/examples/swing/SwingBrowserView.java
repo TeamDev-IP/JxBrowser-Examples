0a6d34dda970ae2a4f74f37743a1d940585f51b6
vladimir ikryanov <vladimir.ikryanov@teamdev.com>
2021-07-20 16:55:45 +0300

    Fix code formatting

--- examples/src/main/java/com/teamdev/jxbrowser/examples/swing/SwingBrowserView.java
+++ examples/src/main/java/com/teamdev/jxbrowser/examples/swing/SwingBrowserView.java
@@ -17,6 +17,7 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package com.teamdev.jxbrowser.examples.swing;
 
 import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
