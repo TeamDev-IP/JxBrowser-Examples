31bd5a6d71245c77e0c5ea429e8cdc94dfa53673
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2022-05-23 12:40:02 +0300

    Don't crash the browser intentionally

--- examples/src/main/java/com/teamdev/jxbrowser/examples/swing/SwingBrowserView.java
+++ examples/src/main/java/com/teamdev/jxbrowser/examples/swing/SwingBrowserView.java
@@ -50,6 +50,6 @@ public final class SwingBrowserView {
             frame.setVisible(true);
         });
 
-        browser.navigation().loadUrl("chrome://crash/");
+        browser.navigation().loadUrl("https://google.com/");
     }
 }
