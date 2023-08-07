16554f5e76371312cf2198f871df8f228a379e31
Vladyslav Lubenskyi <Vladyslav.lubenskyi@teamdev.com>
2023-08-07 14:07:21 +0200

    Explicitly specify SWT version instead of using the plug-in

---
+++ buildSrc/src/main/kotlin/Swt.kt
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 2023, TeamDev. All rights reserved.
+ *
+ *  Redistribution and use in source and/or binary forms, with or without
+ *  modification, must retain the above copyright notice and the following
+ *  disclaimer.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import org.gradle.api.JavaVersion
+import java.util.*
+
+/**
+ * A dependency on SWT.
+ */
+object Swt {
+
+    private const val groupdId = "org.eclipse.platform"
+
+    val version = if (JavaVersion.current().isJava8) {
+        // This version corresponds to Eclipse 4.16, the last version of the framework that
+        // supported Java 8. Note, that SWT itself may still work on Java 8.
+        "3.114.100"
+    } else {
+        // This version also supports Apple Silicon.
+        "3.124.0" // Eclipse 4.28.0
+    }
+
+    /**
+     * Returns the dependency to SWT for the current platform.
+     */
+    fun platformDependency(): String {
+        val os = System.getProperty("os.name").lowercase()
+        val arch = System.getProperty("os.arch")
+        val isArm = "aarch64" == arch || "arm" == arch
+        val artifactId = when {
+            os.contains("win") -> "org.eclipse.swt.win32.win32.x86_64"
+            os.contains("linux") -> {
+                if (isArm) {
+                    "org.eclipse.swt.gtk.linux.aarch64"
+                } else {
+                    "org.eclipse.swt.gtk.linux.x86_64"
+                }
+            }
+            os.contains("mac") -> {
+                if (isArm) {
+                    "org.eclipse.swt.cocoa.macosx.aarch64"
+                } else {
+                    "org.eclipse.swt.cocoa.macosx.x86_64"
+                }
+            }
+            else -> {
+                throw IllegalStateException("Unexpected operating system")
+            }
+        }
+        return "$groupdId:${artifactId}:$version"
+    }
+}
