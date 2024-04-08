58c2c420bb12a4542412126e40a064d50bb16e6f
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2024-04-08 17:56:17 +0200

    Add a JAR file for `JarProtocol` snippet

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,6 +113,9 @@ subprojects {
 
         // Depend on Guava for the Resources utility class used for loading resource files into strings.
         implementation("com.google.guava:guava:$guavaVersion")
+
+        // This file is used by `JarProtocol` example.
+        runtimeOnly(files(rootDir.resolve("examples/src/main/resources/tiny-website.jar")))
     }
 
     tasks.withType<JavaExec> {
