f53d5a33310ad3c24d8b94dfd8ebc9a64fbd46ee
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2024-05-20 14:31:41 +0200

    Fix `SelectClientCertificate` for Kotlin

--- examples/src/main/kotlin/com/teamdev/jxbrowser/examples/SelectClientCertificate.kt
+++ examples/src/main/kotlin/com/teamdev/jxbrowser/examples/SelectClientCertificate.kt
@@ -21,20 +21,16 @@
 package com.teamdev.jxbrowser.examples
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.singleWindowApplication
 import com.teamdev.jxbrowser.browser.callback.SelectClientCertificateCallback
 import com.teamdev.jxbrowser.compose.BrowserView
@@ -42,8 +38,6 @@ import com.teamdev.jxbrowser.dsl.Engine
 import com.teamdev.jxbrowser.dsl.browser.navigation
 import com.teamdev.jxbrowser.dsl.register
 import com.teamdev.jxbrowser.engine.RenderingMode
-import com.teamdev.jxbrowser.net.tls.ClientCertificate
-import com.teamdev.jxbrowser.net.tls.SslPrivateKey
 import java.security.cert.X509Certificate
 import kotlin.jvm.optionals.getOrNull
 
@@ -60,38 +54,32 @@ fun main() = singleWindowApplication(title = "Select client SSL certificate") {
     val engine = remember { Engine(RenderingMode.OFF_SCREEN) }
     val browser = remember { engine.newBrowser() }
 
-    var x509Certificates by remember { mutableStateOf(emptyList<X509Certificate>()) }
-    var onCertificateSelected by remember { mutableStateOf<(X509Certificate) -> Unit>({ }) }
+    var certificates by remember { mutableStateOf(emptyList<X509Certificate>()) }
+    var onSelected by remember { mutableStateOf<(Int) -> Unit>({ }) }
 
     BrowserView(browser)
 
-    if (x509Certificates.isNotEmpty()) {
-        SelectionDialog(
-            certificates = x509Certificates,
-            onSelected = onCertificateSelected
+    if (certificates.isNotEmpty()) {
+        AlertDialog(
+            onDismissRequest = { /* Selection is mandatory. */ },
+            modifier = Modifier.size(800.dp, 300.dp),
+            text = { Text("Select a certificate: ") },
+            buttons = { SelectionList(certificates, onSelected) }
         )
     }
 
     LaunchedEffect(Unit) {
         browser.register(SelectClientCertificateCallback { params, tell ->
-            val certificates = params.certificates()
+            certificates = params.certificates()
+                .mapNotNull { it.toX509Certificate().getOrNull() }
+
             if (certificates.isEmpty()) {
                 tell.cancel()
             }
 
-            x509Certificates = certificates
-                .map { it.toX509Certificate() }
-                .mapNotNull { it.getOrNull() }
-
-            onCertificateSelected = { selectedCertificate ->
-
-                x509Certificates = emptyList()
-
-                val privateKey = SslPrivateKey.of(selectedCertificate.encoded)
-                val clientCertificate = ClientCertificate.of(selectedCertificate, privateKey)
-
-                // Pass the selected certificate to the engine.
-                tell.select(clientCertificate)
+            onSelected = { index ->
+                certificates = emptyList()
+                tell.select(index)
             }
         })
 
@@ -100,47 +88,25 @@ fun main() = singleWindowApplication(title = "Select client SSL certificate") {
 }
 
 @Composable
-private fun SelectionDialog(
-    certificates: List<X509Certificate>,
-    onSelected: (X509Certificate) -> Unit,
-) = Dialog(
-    onDismissRequest = { /* Selection is mandatory. */ },
-    properties = DialogProperties(usePlatformDefaultWidth = false)
-) {
-    Card(
-        modifier = Modifier.size(600.dp, 300.dp),
-        shape = RoundedCornerShape(16.dp),
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Text("Select a certificate: ")
-            SelectionList(certificates, onSelected)
-        }
-    }
-}
-
-@Composable
 private fun SelectionList(
     certificates: List<X509Certificate>,
-    onSelected: (X509Certificate) -> Unit
+    onSelected: (Int) -> Unit
 ) {
-    var selected by remember { mutableStateOf<X509Certificate?>(null) }
+    var selected by remember { mutableStateOf(-1) }
     Column {
-        certificates.forEach { certificate ->
+        certificates.forEachIndexed { index, certificate ->
             Text(
                 text = certificate.subjectX500Principal.name,
                 modifier = Modifier
                     .selectable(
-                        selected = certificate == selected,
+                        selected = index == selected,
                         onClick = {
-                            selected = certificate
-                            onSelected(certificate)
+                            selected = index
+                            onSelected(index)
                         }
                     )
                     .background(
-                        if (selected == certificate) Color.Gray
+                        if (selected == index) Color.Gray
                         else Color.Transparent
                     )
                     .padding(8.dp)
