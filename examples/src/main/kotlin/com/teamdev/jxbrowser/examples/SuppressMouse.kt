f47b1b12225165a3302c3c2b81fdcc9ba665f19c
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2026-01-06 17:40:34 +0100

    Update copyright year to 2026 (#208)

--- examples/src/main/kotlin/com/teamdev/jxbrowser/examples/SuppressMouse.kt
+++ examples/src/main/kotlin/com/teamdev/jxbrowser/examples/SuppressMouse.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2025, TeamDev. All rights reserved.
+ *  Copyright 2026, TeamDev. All rights reserved.
  *
  *  Redistribution and use in source and/or binary forms, with or without
  *  modification, must retain the above copyright notice and the following
